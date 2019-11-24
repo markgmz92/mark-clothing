5b91f593afbd72ae49843b306625420ae844ec7f
markgmz92 <markgmz92@gmail.com>
2019-11-24 23:08:03 +0800

    Created shop data and page

---
+++ src/pages/shop/shop.data.js
@@ -0,0 +1,47 @@
+const SHOP_DATA = [
+    {
+        id: 1, 
+        title: 'Hats', 
+        routeName: 'hats',
+        items: [
+            {
+                id: 1, 
+                name: 'Brown Brim',
+                imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
+                price: 25
+            },
+            {
+                id: 2, 
+                name: 'Blue Beanie', 
+                imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
+                price: 18
+            },
+            {
+                id: 3,
+                name: 'Black & White Longsleeve',
+                imageUrl: 'https://i.ibb.co/RvwnBL8/long-sleeve.png',
+                price: 25
+            },
+            {
+                id: 4,
+                name: 'Pink T-shirt',
+                imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
+                price: 25
+            },
+            {
+                id: 5,
+                name: 'Jean Long Sleeve',
+                imageUrl: 'https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png',
+                price: 40
+            },
+            {
+                id: 6, 
+                name: 'Burgundy T-shirt',
+                imageUrl:'https://i.ibb.co/mh3VM1f/polka-dot-shirt.png',
+                price: 25
+            }
+        ]
+    }
+]
+
+export default SHOP_DATA;
