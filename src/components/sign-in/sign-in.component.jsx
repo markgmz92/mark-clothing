75b00b1a4767d2e4e90b1c20d748d41783220498
markgmz92 <markgmz92@gmail.com>
2019-11-28 21:50:15 +0800

    Updated styling of sign-in/sign-up page

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import CustomButton from '../custom-button/custom-button.component'
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+import { signInWithGoogle } from '../../firebase/firebase.utils';
+
 
 
 import './sign-in.styles.scss';
@@ -50,7 +52,10 @@ class SignIn extends React.Component {
             label='password'
             required
           />
+          <div className='buttons'>
           <CustomButton type='submit'> Sign In</CustomButton>
+          <CustomButton onClick={ signInWithGoogle } isGoogleSignIn > Sign In With Google</CustomButton>
+          </div>
         </form>
       </div>
     );
